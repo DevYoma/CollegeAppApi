085907406edc2547dd492fb451b20a1a6707c104
Ogheneyoma <Lawrenceyoma@gmail.com>
2024-04-03 13:33:26 +0100

    Add project files.

---
+++ CollegeApp/Models/CollegeRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace CollegeApp.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>() {
+            new Student
+            {
+                Id = 1,
+                StudentName = "Ogheneyoma", 
+                Email = "lawrenceyoma@gmail.com", 
+                Address = "Remote"
+            }, 
+            new Student
+            {
+                Id=2,
+                StudentName = "Michael", 
+                Email = "michael@gmail.com", 
+                Address = "Lagos"
+            }
+        };
+    }
+}
