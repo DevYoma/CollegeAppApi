86113efa469a85803175821e1b050b4763ca7ca9
Ogheneyoma <Lawrenceyoma@gmail.com>
2024-04-04 11:57:33 +0100

    refactor: changed the return type of the actions to use ActionResult<>

--- CollegeApp/Controllers/StudentController.cs
+++ CollegeApp/Controllers/StudentController.cs
@@ -9,16 +9,68 @@ namespace CollegeApp.Controllers
     public class StudentController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Student> GetStudents()
+        [Route("All", Name ="GetAllStudents")]
+        public ActionResult<IEnumerable<Student>> GetStudents()
         {
-            return CollegeRepository.Students;
+            return Ok(CollegeRepository.Students);
 
         }
 
-        [HttpGet("{id:int}")]
-        public Student GetStudentById(int id)
+        [HttpGet("{id:int}", Name ="GetStudentById")]
+        public ActionResult<Student> GetStudentById(int id)
         {
-            return CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+            if(id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var student = CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+            if(student == null)
+            {
+                return NotFound($"The student with id {id} not found");
+            }
+
+            return Ok(student);
+        }
+
+        [HttpGet("{name:alpha}", Name = "GetStudentByName")]
+        //[Route("{name}", Name ="GetStudentByName")]
+        public ActionResult<Student> GetStudentByName(string name)
+        {
+            //checking if string is Null or Empty
+            if(string.IsNullOrEmpty(name))
+            {
+                return BadRequest();
+            }
+
+            var student = CollegeRepository.Students.Where(n => n.StudentName == name).FirstOrDefault();
+            if(student == null)
+            {
+                return NotFound($"The student with name {name} not found");
+            }
+
+            return Ok(student);
+        }
+
+        [HttpDelete("{id}", Name ="DeleteStudentById")]
+        public ActionResult<bool> DeleteStudent(int id)
+        {
+            // BAD REQUEST
+            if(id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var student = CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+            // NOT FOUND
+            if(student == null)
+            {
+                return NotFound($"The Student with the id {id} not found");
+            }
+
+            CollegeRepository.Students.Remove(student);
+             // OK - 200 
+            return Ok(true);
         }
     }
 }
